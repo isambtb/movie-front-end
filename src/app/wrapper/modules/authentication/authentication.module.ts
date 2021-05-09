191374801eac0c46a46795394eb4aaeba3b5dfa5
BOUTAIB Najib <boutaib.najib1@gmail.com>
2021-05-09 15:48:23 +0200

    STyling sign in page

--- src/app/wrapper/modules/authentication/authentication.module.ts
+++ src/app/wrapper/modules/authentication/authentication.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import {MatCardModule} from '@angular/material/card';
 import { AuthenticationRoutingModule } from './authentication-routing.module';
 import { SignInComponent } from './components/sign-in/sign-in.component';
 import { ReactiveFormsModule } from '@angular/forms';
@@ -19,13 +19,15 @@ import { FormlyMaterialModule } from '@ngx-formly/material';
     FormlyModule.forRoot({ extras: { lazyRender: true } }),
     FormlyMaterialModule,
     BrowserAnimationsModule,
-    AuthenticationRoutingModule
+    AuthenticationRoutingModule,
+    MatCardModule,
   ],
   exports: [
     ReactiveFormsModule,
     FormlyModule,
     FormlyMaterialModule,
     BrowserAnimationsModule,
+    MatCardModule,
     SignInComponent,
   ]
 })
